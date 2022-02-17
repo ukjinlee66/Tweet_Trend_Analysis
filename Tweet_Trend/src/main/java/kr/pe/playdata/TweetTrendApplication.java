e24d2bb08c49c5586e38f77b161c23923ce494be
Youkjin Lee <ukjinlee66@gmail.com>
2022-02-17 16:44:13 +0900

    project update

---
+++ Tweet_Trend/src/main/java/kr/pe/playdata/TweetTrendApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.playdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetTrendApplication.class, args);
+	}
+
+}

2a5551994ba01a93e69f9dde397e051bf1dee4b7
ih-tjpark <hahxowns@naver.com>
2022-02-23 02:22:15 +0900

    img update

--- Tweet_Trend/src/main/java/kr/pe/playdata/controller/maincontroller.java
+++ Tweet_Trend/src/main/java/kr/pe/playdata/controller/maincontroller.java
@@ -1,10 +1,24 @@
 package kr.pe.playdata.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.Base64;
+import java.util.Base64.Decoder; 
+import java.util.Base64.Encoder;
+
+import static org.apache.commons.io.FileUtils.openInputStream;
+import static org.apache.commons.io.FileUtils.writeByteArrayToFile;
+import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
@@ -32,8 +46,48 @@ public class maincontroller
         return str;
     }
 	@GetMapping("/")
-	public String mainPage(Model model) 
-	{
+	public String mainPage(Model model) throws Exception 
+	{	
+		
+		JSONParser parser = new JSONParser();
+
+		FileReader reader = new FileReader("c:/json/img.json");
+
+		JSONObject jsonObject = (JSONObject) parser.parse(reader);
+
+		String img_name = (String) jsonObject.get("img");
+		
+		byte[] byteArrray = img_name.getBytes();
+		System.out.println(byteArrray);
+		
+		//byte[] decodedBytes = Base64.getMimeDecoder().decode(img_name);
+		
+		//String directory=servletContext.getRealPath("/")+"images/sample.jpg";
+		//FileUtils.writeByteArrayToFile(new File(outputFileName), decodedBytes);
+		
+		String absolutePath = new File("src/main/resources").getAbsolutePath() + "/"+"img.png";
+	    
+		FileOutputStream output = new FileOutputStream(absolutePath);
+		//System.out.println(decodedBytes);
+		//System.out.println(output);
+		;
+		//fos.write(decodedBytes);
+		//fos.close();
+		
+	    String text = "ktko"; 
+	    byte[] targetBytes = text.getBytes(); 
+	    Encoder encoder = Base64.getEncoder(); 
+	    byte[] encodedBytes = encoder.encode(targetBytes); 
+	    Decoder decoder = Base64.getDecoder(); 
+	    byte[] decodedBytes = decoder.decode(img_name.getBytes("UTF-8")); 
+	    System.out.println("인코딩 전 : " + text); 
+	    System.out.println("인코딩 text : " + new String(encodedBytes)); 
+	    System.out.println("디코딩 text : " + new String(decodedBytes));
+	    
+	    FileUtils.writeByteArrayToFile(new File("img.png"), decodedBytes);
+	    output.write(decodedBytes);
+	    output.close();
+	  
 		return "MainPage";
 	}
 }

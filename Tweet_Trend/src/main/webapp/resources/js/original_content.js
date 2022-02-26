58e111fc7d68cbfdda721c8f9999c09f64640c79
dldydrhkd <sksda4614@naver.com>
2022-02-26 15:38:12 +0900

    content 중복제거
    twitter content 중복으로 안 들어오게끔 처리

--- Tweet_Trend/src/main/webapp/resources/js/original_content.js
+++ Tweet_Trend/src/main/webapp/resources/js/original_content.js
@@ -2,7 +2,6 @@ window.onload = function() {
 	setInterval(async function() {
 		var list_data = await loadList();
 		var idx = list_data.length-1;
-		console.log(list_data);
 		createDIV(list_data[idx]["content"],list_data[idx]["sentiment"],idx);
 	}, 1000);
 };
@@ -19,30 +18,27 @@ function createDIV(content, sentiment) {
 		back_color = "#dc3545";
 	}
 	newDiv.innerHTML = `<div class="d-flex text-muted pt-3">
-								<svg class="bd-placeholder-img flex-shrink-0 me-2 rounded"
+							<svg class="bd-placeholder-img flex-shrink-0 me-2 rounded"
 									width="32" height="32" xmlns="http://www.w3.org/2000/svg"
 									role="img" aria-label="Placeholder: 32x32"
-									preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%"
-										fill="${back_color}"/></svg>
-								<p class="pb-3 mb-0 small lh-sm border-bottom">
-									<strong class="d-block text-gray-dark">@username</strong>${content}
-								</p>
-							</div>`
+									preserveAspectRatio="xMidYMid slice" focusable="false">
+									<rect width="100%" height="100%" fill="${back_color}"/></svg>
+							<p class="pb-3 mb-0 small lh-sm border-bottom">
+								<strong class="d-block text-gray-dark">@username</strong>${content}
+							</p>
+						</div>`
 	child_len = obj.childElementCount
 	if(child_len>10){
 		obj.removeChild(obj.childNodes[9]);
 	}
-//	if(obj.childNodes[0]!=undefined){
-//		if(obj.childNodes[0].innerHTML!==content){
-//			console.log(obj.childNodes[0].innerHTML);
-//			console.log(content);
-//			obj.insertBefore(newDiv,obj.childNodes[0]);
-//		}
-//	}
-//	else{
-//		obj.insertBefore(newDiv,obj.childNodes[0]);
-//	}
-	obj.insertBefore(newDiv,obj.childNodes[0]);
+	if(obj.childNodes[0]!=undefined){
+		if(obj.childNodes[0].innerHTML!==newDiv.innerHTML){
+			obj.insertBefore(newDiv,obj.childNodes[0]);
+		}
+	}
+	else{
+		obj.insertBefore(newDiv,obj.childNodes[0]);
+	}
 }
 
 loadList = () => {
